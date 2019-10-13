49cc71a8462a654b9ddeac1a2d882fedaddad265
rnaffandi@gmail.com <rnaffandi>
2019-10-13 11:48:21 +0700

    first commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.7.5',
+  'full_app_version' => 'v4.7.5 - build 4118-g03a451240',
+  'build_version' => '4118',
+  'prerelease_version' => '',
+  'hash_version' => 'g03a451240',
+  'full_hash' => 'v4.7.5-10-g03a451240',
+  'branch' => 'master',
+);
